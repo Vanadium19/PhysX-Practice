ccfdef36ac4c24879bf0efbba1b7851d5d29b22e
Vladislav Melnik <GimliVlad@list.ru>
2026-03-29 11:11:44 +0300

    Init

---
+++ PhysX-Practice/PhysX-Practice.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include "PxPhysicsAPI.h"
+
+int main()
+{
+	physx::PxDefaultAllocator allocator;
+	physx::PxDefaultErrorCallback errorCallback;
+	physx::PxFoundation* foundation = PxCreateFoundation(PX_PHYSICS_VERSION, allocator, errorCallback);
+
+	foundation->release();
+
+	return 0;
+}
